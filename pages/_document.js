f1ecc587e7bebb0e4587e98afbcb749971be9db0
rohit rawat <rohitrawatop@gmail.com>
2023-10-12 00:10:29 +0530

    update

---
+++ pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <meta name="google-site-verification" content="3ZOKJtdIrGhlv-LdRSH2Zulk4dqYsj_woeR6IWB0q10" />
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
