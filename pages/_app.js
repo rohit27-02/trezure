a140f90f03f7de48d8bc5c20b9558feefc429003
rohit rawat <rohitrawatop@gmail.com>
2023-10-11 23:23:08 +0530

    google verifivation

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,9 @@ import Footer from '../components/Footer';
 function MyApp({ Component, pageProps }) {
   return  <>
   <Header/>
+  <head>
+  <meta name="google-site-verification" content="3ZOKJtdIrGhlv-LdRSH2Zulk4dqYsj_woeR6IWB0q10" />
+  </head>
   <Component {...pageProps} />
   <Footer/>
   </>
