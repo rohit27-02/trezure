d2a3f358c76feb897157c7fd61039be5647b5fbc
rohit rawat <rohitrawatop@gmail.com>
2023-10-16 15:35:05 +0530

    update

--- pages/api/getfiles.js
+++ pages/api/getfiles.js
@@ -3,7 +3,8 @@ const path = require('path');
 
 // Function to recursively read files from subfolders
 function readFilesFromSubfolders(folderPath, fileNames = []) {
-    const files = fs.readdirSync(path.resolve(folderPath));
+    
+    const files = fs.readdirSync('/var/task/public/collections/home/beds');
     for (const file of files) {
         const filePath = path.join(folderPath, file);
         if (fs.statSync(filePath).isDirectory()) {
@@ -12,6 +13,7 @@ function readFilesFromSubfolders(folderPath, fileNames = []) {
             fileNames.push(filePath);
         }
     }
+    console.log(fileNames)
     return fileNames;
 }
 
