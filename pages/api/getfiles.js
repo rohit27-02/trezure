4e27fdf06a5d412f2dc26f8a6c523917db46c5d2
rohit rawat <rohitrawatop@gmail.com>
2023-10-16 15:51:39 +0530

    c

--- pages/api/getfiles.js
+++ pages/api/getfiles.js
@@ -4,7 +4,7 @@ const path = require('path');
 // Function to recursively read files from subfolders
 function readFilesFromSubfolders(folderPath, fileNames = []) {
     
-    const files = fs.readdirSync(path.join(process.cwd,folderPath));
+    const files = fs.readdirSync(path.join(process.cwd(),folderPath));
     for (const file of files) {
         const filePath = path.join(folderPath, file);
         if (fs.statSync(filePath).isDirectory()) {
