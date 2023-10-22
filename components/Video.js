98deb809ab1dd8e11fb3d2757a7ac03e9545270f
rohit rawat <rohitrawatop@gmail.com>
2023-10-22 12:46:07 +0530

    video caption

--- components/Video.js
+++ components/Video.js
@@ -10,7 +10,7 @@ const Playvideo = () => {
             <div onClick={()=>{setclicked(true)}} className='absolute  '>
                 <h1 className='cursor-pointer hover:text-brown-light transition-all duration-300'>CORPORATE VIDEO</h1>
             </div>
-            <p className='absolute mt-[20rem] text-center text-sm md:w-[50rem] drop-shadow max-sm:hidden tracking-normal '>With an experience of over four decades up our sleeves, our rise into a luxurious furniture segment has been splendid, filled with the richness of talent and magnificence. Shouldering a responsibility to enhance our heritage further, we are committed to serving our clients with only the best in class experience.</p>
+            <p className='absolute mt-[20rem] text-center text-3xl max-sm:text-xl md:w-[50rem] drop-shadow max-sm:hidden tracking-normal '>Elevating Spaces with Affordable Luxury</p>
         </div>
     )
 }
