817cee6daa5f1d87c0751418cf1634f1e6229652
rohit rawat <rohitrawatop@gmail.com>
2023-11-01 18:55:24 +0530

    catalog

---
+++ pages/Catalog.js
@@ -0,0 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+
+const Catalog = () => {
+  return (
+    <div className='w-screen h-screen relative z-50  overflow-hidden'>
+        <img className='absolute inset-0 object-cover' src='/bg.jpg' alt=''/>
+    </div>
+  )
+}
+
+export default Catalog
